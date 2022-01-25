84f4f36c83267eff9ab9d5ef80df38a096472cd6
Asif Ahmed <asifbin8@gmail.com>
2022-01-26 00:18:30 +0600

    commit

--- index.js
+++ index.js
@@ -3,5 +3,33 @@ var number= 2.5667
 console.log(number.toPrecision(1))
 console.log(number.toFixed(1))
 
-console.log(Number(12))
+console.log(Number(11))
 
+var base= parseFloat (prompt("Enter Base: "));
+var height= parseFloat (prompt("Enter Height: "));
+
+var area= (base*height)/2;
+
+document.write("Area: " + area + "<br>");
+
+var farn= parseFloat (prompt("Enter farenheit: "));
+
+var cels= (farn-32)*(5/9);
+
+document.write("Celsius:" + cels + "<br>2");
+
+var cels= parseFloat (prompt("Enter celsius: "));
+
+var farn= (cels*9/5)+32;
+
+document.write("Farenhite:" + farn + "<br>");
+
+var num= 5;
+
+if(num % 2 == 0)
+
+console.log("Even");
+
+else
+
+console.log("Odd");
