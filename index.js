a0d0533d90e912fface75f7a50601d34841f233a
Asif Ahmed <asifbin8@gmail.com>
2022-01-26 13:44:39 +0600

    commit

--- index.js
+++ index.js
@@ -33,3 +33,24 @@ console.log("Even");
 else
 
 console.log("Odd");
+
+var marks= prompt("Enter your marks: ")
+
+if(marks>=80)
+console.log("A+");
+else if(marks>=70)
+console.log("A");
+else if(marks>=60)
+console.log("B+");
+else if(marks>=50)
+console.log("B");
+else if(marks>=40)
+console.log("C");
+else if(marks>=33)
+console.log("D");
+else
+console.log("Fail");
+
+
+
+
