662573955910c6dcbd23ac70f5b9b9bd46999b6a
Asif Ahmed <asifbin8@gmail.com>
2022-01-27 00:05:32 +0600

    commit

--- index.js
+++ index.js
@@ -157,6 +157,24 @@ do{
     i++;
 }while(i<=20)
 
+//break & continue//
+
+for(var i= 1; i<=100; i++){
+    
+    document.write(" " + i)
+    if(i==10){
+        break;
+    }
+}
+
+
+for(var i= 1; i<=100; i++){
+    if(i==10){
+        continue;
+    }  
+    document.write(" " + i)
+}
+
 
 
 
